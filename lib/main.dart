597bfb39bc14730d5cc1ce64ea1dcc0851949392
frapt <wagashi.potato@gmail.com>
2024-06-21 12:27:22 +0900

    sec

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,79 @@ class MyApp2 extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '実践演習0620',
-      home: Scaffold(
-        appBar: AppBar(title: Text('デモアプリ')),
-        floatingActionButton: FloatingActionButton(
-          onPressed: ()=>{}, child: Icon(Icons.air)),
-        body: ConstrainedBox(
-          constraints: BoxConstraints.expand(),
-          child: FlutterLogo()
-          )
-      ),
+      title: '実践演習0620', home: MyHome()
     );
   }
 }
+
+class MyHome extends StatefulWidget{
+  const MyHome({super.key});
+  @override
+  _MyHomeState createState() {
+    return _MyHomeState();
+  }
+}
+
+class _MyHomeState extends State<MyHome>{
+  int count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('デモアプリ')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: ()=>{
+          setState((){
+            count = 0;
+          })
+        }, 
+        child: const Icon(Icons.cancel)
+      ),
+      
+      body: Column(
+        children: [
+          Text("$count",style: TextStyle(fontSize: 80),),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              BinDigit(),
+              BinDigit(),
+              BinDigit(),
+              BinDigit(),
+              BinDigit(),
+              BinDigit(),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            ElevatedButton(
+              onPressed: () {
+
+                setState((){count++;});    //setStateでcountの更新を適用する
+              
+              }, child: const Icon(Icons.plus_one)
+              ),
+            ElevatedButton(
+              onPressed: () {
+
+                setState(() {count--;});
+
+              }, child: const Icon(Icons.exposure_minus_1)
+              ),
+
+            ],)],));
+  }
+}
+
+class BinDigit extends StatelessWidget{
+  const BinDigit({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Text("0");
+  }
+  
+}
+
 
 
 
